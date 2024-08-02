9c91c307977fec291bfd1affc3bccffbfdced2ff
jotadev <mejiajhoan2021@gmail.com>
2024-08-01 23:07:31 -0400

    Add outer options

--- dashboard/api/index.php
+++ dashboard/api/index.php
@@ -9,13 +9,15 @@ require_once "lib/updatePersonal.php";
 require_once "lib/updatePassword.php";
 require_once "lib/getData.php";
 require_once "lib/updateAditional.php";
+require_once "lib/getProducts.php";
+require_once "lib/getClients.php";
 
 $action = $_GET["action"];
 $slot = $_GET["slot"];
 $message = array("result" => false, "description" => "");
 
-if ($slot === "profile") {
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if ($slot === "profile") {
         if($action === "updatePersonal"){
             echo updateName($pdo, $_POST, $message);
         }
@@ -26,9 +28,15 @@ if ($slot === "profile") {
             echo updateAditional($pdo, array_merge($_FILES, $_POST), $message);
         }
     }
-    if($_SERVER["REQUEST_METHOD"] == "GET"){
-        if(isset($_GET["ci"])){
-            echo getUser($pdo, $_GET["ci"]);
-        }
+}
+if($_SERVER["REQUEST_METHOD"] == "GET"){
+    if(isset($_GET["ci"]) && $slot === "profile"){
+        echo getUser($pdo, $_GET["ci"]);
+    }
+    if($slot === "products"){
+        echo getProducts($pdo);
+    }
+    if($slot === "clients"){
+        echo getClients($pdo);
     }
 }
