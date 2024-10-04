6dbc50a505112b2745be336e9d220f494b3d0d85
jdev Wind <mejiajhoan2021@gmail.com>
2024-10-04 03:45:49 -0430

    add: jsdoc

--- dashboard/lib/dom-selector.js
+++ dashboard/lib/dom-selector.js
@@ -1,5 +1,14 @@
+/**
+ * 
+ * @param {string} element 
+ * @returns HTMLElement
+ */
 const $ = (element) => document.querySelector(element)
-
+/**
+ * 
+ * @param {string} element 
+ * @returns ListHTMLElement
+ */
 const $$ = (element) => document.querySelectorAll(element)
 
 export {
